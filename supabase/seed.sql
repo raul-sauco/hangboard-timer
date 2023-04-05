9824da29e01b74cddc66ed8d7b0023daf48586fa
Raul Sauco <sauco.raul@gmail.com>
2023-04-05 20:09:00 +0200

    Create initial schema, add seed

---
+++ supabase/seed.sql
@@ -0,0 +1,5 @@
+insert into public.session (session_name, details)
+values (
+    'Warm Up',
+    '{"name":"Warmup","sets":[{"hangs":[{"hangTime":30,"restTime":30},{"hangTime":40,"restTime":30},{"hangTime":40,"restTime":30}],"restTimeSet":300},{"hangs" :[{"hangTime":30,"restTime":30},{"hangTime":40,"restTime":30},{"hangTime":40,"restTime":30}],"restTimeSet":300}]}'
+  );
