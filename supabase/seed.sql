12400aafc5f31f46a1d9e1d27f8189c60a64526b
Raul Sauco <sauco.raul@gmail.com>
2023-04-08 09:30:19 +0200

    Configure Supabase deep linking

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,4 +1,4 @@
-insert into public.session (session_name, details)
+insert into public.sessions (session_name, details)
 values (
     'Warm Up',
     '{"name":"Warmup","sets":[{"hangs":[{"hangTime":30,"restTime":30},{"hangTime":40,"restTime":30},{"hangTime":40,"restTime":30}],"restTimeSet":300},{"hangs" :[{"hangTime":30,"restTime":30},{"hangTime":40,"restTime":30},{"hangTime":40,"restTime":30}],"restTimeSet":300}]}'
